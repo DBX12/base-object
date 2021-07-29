98a5223be05f7294aa583338857f473eab0d0d29
DBX12 <dbx12@dbx12.de>
2021-07-29 11:42:17 +0200

    Initial

---
+++ tests/support/UnitTester.php
@@ -0,0 +1,26 @@
+<?php /** @noinspection PhpIllegalPsrClassPathInspection This is needed for codeception to work properly */
+namespace tests;
+
+/**
+ * Inherited Methods
+ * @method void wantToTest($text)
+ * @method void wantTo($text)
+ * @method void execute($callable)
+ * @method void expectTo($prediction)
+ * @method void expect($prediction)
+ * @method void amGoingTo($argumentation)
+ * @method void am($role)
+ * @method void lookForwardTo($achieveValue)
+ * @method void comment($description)
+ * @method void pause()
+ *
+ * @SuppressWarnings(PHPMD)
+*/
+class UnitTester extends \Codeception\Actor
+{
+    use \tests\_generated\UnitTesterActions;
+
+    /**
+     * Define custom actions here
+     */
+}
